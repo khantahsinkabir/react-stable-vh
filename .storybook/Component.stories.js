6ddb38c81e6a7eb4e19fa15a5da90a0e395a0f4b
khantahsinkabir <khantahsinkabir@gmail.com>
2022-12-03 10:31:49 +0600

    Auto Git Commit

---
+++ .storybook/Component.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import StableVH from '../src/index.js';
+
+export default {
+	component: StableVH,
+	title: 'Counter'
+};
+
+export const Placeholder = () => <StableVH />;
